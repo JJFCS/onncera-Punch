353995fcde8cd665ed6af3689a8b7bbaff9f6f64
onncera-shogun <onncera@localhost.com>
2024-05-12 09:41:21 +0800

    Formatting

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,12 @@
-local wezterm = require "wezterm" -- pull in the wezterm API
-local config  = {}                -- this table will hold the configuration
+local wezterm = require "wezterm" -- PULL IN THE WEZTERM API
+local config  = {}                -- THIS TABLE WILL HOLD THE CONFIGURATION
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
+-- config_builder will help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+-- This is where you actually apply your config choices.
 -- changing the color scheme: (https://wezfurlong.org/wezterm/colorschemes/index.html), for example
 -- config.color_scheme = 'Batman'
 
@@ -17,18 +16,17 @@ config.color_scheme = 'Japanesque'
 config.font         = wezterm.font_with_fallback {
 	{ family = 'MartianMono Nerd Font', weight = 'Regular' },
 	{ family = 'Iosevka Nerd Font',     weight = 'Medium'  },
-	'Iosevka Term Slab Medium',
 	'nonicons',
 }
 config.font_size           = 9     -- can use fractional point sizes, such as 13.3
 
 config.line_height         = 1     -- scales computed line height to adjust spacing between successive rows  of text
-config.cell_width          = 0.8   -- scales computed cell width  to adjust spacing between successive cells of text
+config.cell_width          = 1     -- scales computed cell width  to adjust spacing between successive cells of text
 
 config.underline_thickness =  2    -- overrides the base thickness of underlines
 config.underline_position  = -1    -- negative number to offset the baseline of the font
 
--- I don't like putting anything at the edge if I can help it.
+-- I do not like putting anything at the edge if I can help it
 config.enable_scroll_bar = false
 config.window_padding    = {
 	left   = 0,
